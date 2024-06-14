f3ff52a173447db2664cab8262aaf375b54bbff8
Leaders2022 <georgeobby@gmail.com>
2024-06-14 11:24:41 +0300

    python2

---
+++ functionswithparameters.py
@@ -0,0 +1,6 @@
+def majina(name, age, gender): #Here they are called parameters
+    print(f"My name is {name}, i'm {age} yea old and i'm {gender}")
+majina("George", 50, "Male") #Here they are called arguments.
+majina("Ken", 56, "Male")
+majina("John", 50, "Male")
+majina("Diana", 56, "female")
