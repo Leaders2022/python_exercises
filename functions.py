53317a7dda76860b676aebcd4889c85c425174ef
Leaders2022 <georgeobby@gmail.com>
2024-06-14 11:06:36 +0300

    python2

---
+++ functions.py
@@ -0,0 +1,10 @@
+def myfunction():
+    print("This is my function")
+myfunction()  #this is calling a function
+myfunction()
+
+def addtwonumbers():
+    num=int(input("Enter the first number"))
+    num2=int(input("Enter the second number: "))
+    print(f"The sum of {num} and {num2} is {num+num2}")
+addtwonumbers()
