60dd493df685d05ac6bf3d868195dcd81fbd6a3d
Leaders2022 <georgeobby@gmail.com>
2024-06-19 08:53:28 +0300

    python commit

--- oopexercise.py
+++ oopexercise.py
@@ -19,8 +19,17 @@ class Cars:
         print(f"My dream car is {self.model}, manufactured in {self.year_of_manufacture} being {self.type}, {self.color} in color")
 
         #objects
-my_cars= Cars(model="Toyota", year_of_manufacture="2020", type="Vanguard", color="Black")
+my_cars= Cars("Toyota", "2020", "Vanguard", "Black")
 my_cars.display()
-
+my_cars2=Cars("Toyota", "2023", "Rav4","White")
+my_cars2.display()
+my_cars3= Cars("Mercedes", "2019", "Benz E Class", "grey")
+my_cars3.display()
+my_cars4= Cars("Toyota",  "2018", "Land Cruiser", "black")
+my_cars4.display()
+my_cars5= Cars("Toyota", "2021", "Prado", "black")
+my_cars5.display()
+my_cars6= Cars("Toyota", "2023", "Landrover", "black")
+my_cars6.display()
 
 
